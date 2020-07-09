765c812db43c9c6de6c3da6c2e239f296e020ff1
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-07-09 16:53:06 -0300

    Fixed erro on zip command

--- build/build.sh
+++ build/build.sh
@@ -12,7 +12,7 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     telegram -M "`printenv ROM_NAME`: Build completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
     SYNC_START=$(date +"%s")
-    telegram -M "`printenv ROM_NAME`: Zipping output started"
+    telegram -M "`printenv ROM_NAME`: Zipping GSI files started"
 
     export date2=`date +%Y%m%d%H%M`
     export sourcever2=`cat ./ErfanGSIs/ver`
@@ -22,8 +22,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
                
     curl -sL https://git.io/file-transfer | sh
                
-    zip -r $ROM-AB-$sourcever2-$date2-ErfanGSI.7z *-AB-*.img
-    zip -r $ROM-Aonly-$sourcever2-$date2-ErfanGSI.7z *-Aonly-*.img
+    zip -r "$ROM-AB-$sourcever2-$date2-ErfanGSI.7z" "*-AB-*.img"
+    zip -r "$ROM-Aonly-$sourcever2-$date2-ErfanGSI.7z" "*-Aonly-*.img"
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
@@ -32,16 +32,16 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     SYNC_START=$(date +"%s")
     telegram -M "`printenv ROM_NAME`: Upload started"
 
-    ./transfer $MIR $ROM-Aonly-$sourcever2-$date2-ErfanGSI.7z
-    ./transfer $MIR $ROM-AB-$sourcever2-$date2-ErfanGSI.7z
+    ./transfer $MIR "$ROM-Aonly-$sourcever2-$date2-ErfanGSI.7z"
+    ./transfer $MIR "$ROM-AB-$sourcever2-$date2-ErfanGSI.7z"
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
-    telegram -M "`printenv ROM_NAME`: Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -M "`printenv ROM_NAME`: Upload completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
-    telegram -M "HavocOS: Uploading completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+    telegram -M "`printenv ROM_NAME`: Upload completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
     SYNC_START=$(date +"%s")
     telegram -M "`printenv ROM_NAME` GSI For A/AB Devices \n\n`printenv CAT_FILE`"
