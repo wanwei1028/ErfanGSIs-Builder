7bbfc297168421774a4b870986d66cfcabe56e8b
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-07-10 11:37:52 -0300

    [build] OxygenOS:HydrogenOS

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -7,19 +7,25 @@ with open('Aonly.txt') as f:
     os.environ['CAT_FILE'] = f.read()
 
 rom = os.getenv('ROM_NAME')
+zip = os.getenv('ZIP_NAME')
 cat = os.getenv('CAT_FILE')
+romurl = os.getenv('ROM_URL')
 da = os.getenv('DOWNLOAD_A')
 dab = os.getenv('DOWNLOAD_AB')
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
     client.send_message(
-        text=f"""<b>{rom} GSI For A/AB Devices</b>
+        text=f"""<b>{zip} GSI For A/AB Devices</b>
+
+<b>Firmware Base:</b> <a href="{romurl}">HERE</a>
 
 <b>Information:</b>
 <code>{cat}</code>
-        
+
 <b>Download A-Only:</b> <a href="{da}">HERE</a>
 <b>Download A/B:</b> <a href="{dab}">HERE</a>
+
+<b>Thanks to: <a href="https://github.com/erfanoabdi/ErfanGSIs/graphs/contributors">Contributors List</a>
 
 <a href="https://github.com/erfanoabdi/ErfanGSIs">Ported using ErfanGSIs Tool</a>""",
         chat_id=os.getenv('CHAT_ID'),
