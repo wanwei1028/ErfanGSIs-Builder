3a7b41058d775ad4f879cc0f5721c5cc2701f6e8
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-07-10 09:37:03 -0300

    Maybe now the output will be sent to Telegram correctly

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -1,10 +1,22 @@
 import os
 import pyrogram
 
+with open('Aonly.txt') as f:
+    os.environ['CAT_FILE'] = f.read()
+
+rom = os.getenv('ROM_NAME')
+cat = os.getenv('CAT_FILE')
+da = os.getenv('DOWNLOAD_A')
+dab = os.getenv('DOWNLOAD_AB')
+
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
-        client.send_message(
-            text=os.getenv('CAT_FILE'),
-            chat_id=os.getenv('CHAT_ID'),
-            parse_mode="markdown",
-            disable_web_page_preview=True
-        )
+    client.send_message(
+        text=f"""*{rom} GSI For A/AB Devices*
+
+        {cat}
+            
+        {da} // {dab}""",
+        chat_id=os.getenv('CHAT_ID'),
+        parse_mode="markdown",
+        disable_web_page_preview=True
+    )
