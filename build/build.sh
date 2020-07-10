e210afc56ec5b716d78decfa912f485085077a44
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-07-09 22:36:19 -0300

    I definitely don't know what else to try

--- build/build.sh
+++ build/build.sh
@@ -32,8 +32,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     SYNC_START=$(date +"%s")
     telegram -M "`printenv ROM_NAME`: Upload started"
 
-    export DOWNLOAD_A=$(./transfer $MIR "$ROM_NAME-Aonly-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')
-    export DOWNLOAD_AB=$(./transfer $MIR "$ROM_NAME-AB-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+')
+    export DOWNLOAD_A=`./transfer $MIR "$ROM_NAME-Aonly-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+'`
+    export DOWNLOAD_AB=`./transfer $MIR "$ROM_NAME-AB-$sourcever2-$date2-ErfanGSI.7z" | grep -o -P '(?<=Download Link: )\S+'`
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
@@ -42,7 +42,7 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_URL $ROM_NAME
     export CAT_FILE=`cat ErfanGSIs/output/*-Aonly-*.txt`
 
     SYNC_START=$(date +"%s")
-    telegram -M "`printenv ROM_NAME` GSI For A/AB Devices\n\n`printenv CAT_FILE`"
+    telegram -M "`printenv ROM_NAME` GSI For A/AB Devices\n\n `printenv CAT_FILE`"
 
     SYNC_START=$(date +"%s")
-    telegram -M "$(DOWNLOAD_A) // $(DOWNLOAD_AB)"
+    telegram -M "`printenv DOWNLOAD_A` // `printenv DOWNLOAD_AB`"
